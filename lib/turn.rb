6201c29c7085e18d546ed01f077051604b9b8877
ak <aravindakrishnan92@gmail.com>
2019-02-24 19:44:58 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -26,7 +26,7 @@ def move
 end
 
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-puts "Welcome to Tic Tac Toe!"
-display_board(board)
-turn(board)
+# board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+# puts "Welcome to Tic Tac Toe!"
+# display_board(board)
+# turn(board)
